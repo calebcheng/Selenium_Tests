c4e94a244ca69bb1bca5fd1c9647cdb3c4c94eab
Caleb Cheng <clcheng29@gmail.com>
2018-01-23 18:41:06 +1000

    add fswatch.sh

---
+++ listener/fswatch.sh
@@ -0,0 +1,2 @@
+chmod 777 openTerminal.sh
+fswatch -o /Users/lizhughes/testEnvironment/Selenium_Tests/listener/kickStart | xargs -n1 "/Users/lizhughes/testEnvironment/Selenium_Tests/listener/openTerminal.sh"
